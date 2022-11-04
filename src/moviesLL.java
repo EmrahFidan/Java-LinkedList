c31faeeaa258f6f8023f0842764bf06ebb714159
Emrahfidan <201805070@stu.adu.edu.tr>
2022-11-04 14:52:14 +0300

    created Node structure

--- src/moviesLL.java
+++ src/moviesLL.java
@@ -1,9 +1,31 @@
 
 public class moviesLL {
 
+	// I created a node structure
+	static class Node {
+
+		String movieName;
+		String movieDirectory;
+		String movieCategory;
+		double movieIMDBrate;
+
+		Node next;
+
+		// constructor
+		public Node(String movieName, String movieDirectory, String movieCategory, double movieIMDBrate) {
+			super();
+			this.movieName = movieName;
+			this.movieDirectory = movieDirectory;
+			this.movieCategory = movieCategory;
+			this.movieIMDBrate = movieIMDBrate;
+
+			next = null;
+
+		}
+	}
+
+		
 	public static void main(String[] args) {
-		
-		
 
 	}
 
